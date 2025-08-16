1849b7a1d134f6eb59b2f02bcc5d19d84c164fd5
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2025-08-16 13:55:54 +0530

    feat(gin): basic server handler setup complete

---
+++ cmd/api/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/shekharsikku/go-gin-rest-api/internal/database"
+	"github.com/shekharsikku/go-gin-rest-api/internal/env"
+
+	_ "github.com/joho/godotenv/autoload"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type application struct {
+	port      int
+	jwtSecret string
+	models    database.Models
+}
+
+func main() {
+	db, err := sql.Open("sqlite3", "./data.db")
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	defer db.Close()
+
+	models := database.NewModels(db)
+
+	app := &application{
+		port:      env.GetEnvInt("PORT", 4000),
+		jwtSecret: env.GetEnvString("JWT_SECRET", "secret@gin1.10"),
+		models:    models,
+	}
+
+	if err := app.serve(); err != nil {
+		log.Fatal(err)
+	}
+}
