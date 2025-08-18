2533c00e599ebdfc208255877b386258c7c9c6cd
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2025-08-18 08:52:03 +0530

    feat(api): update fields and update delete query

--- internal/database/events.go
+++ internal/database/events.go
@@ -13,12 +13,12 @@ type EventModel struct {
 }
 
 type Event struct {
-	Id          int    `json:"id"`
-	Owner       int    `json:"owner" binding:"required"`
-	Name        string `json:"name" binding:"required,min=3"`
-	Description string `json:"description" binding:"required,min=10"`
-	Location    string `json:"location" binding:"required,min=3"`
-	Date        string `json:"date" binding:"required,datetime=2006-01-02"`
+	Id          int       `json:"id"`
+	Owner       int       `json:"owner" binding:"required"`
+	Name        string    `json:"name" binding:"required,min=3"`
+	Description string    `json:"description" binding:"required,min=10"`
+	Location    string    `json:"location" binding:"required,min=3"`
+	DateTime    time.Time `json:"datetime" binding:"required" time_format:"2006-01-02 15:04:05"`
 }
 
 func (em *EventModel) Insert(event *Event) error {
@@ -30,7 +30,7 @@ func (em *EventModel) Insert(event *Event) error {
 
 	id := utils.GenerateUniqueID()
 
-	return em.DB.QueryRowContext(ctx, query, id, event.Owner, event.Name, event.Description, event.Date, event.Location).Scan(&event.Id)
+	return em.DB.QueryRowContext(ctx, query, id, event.Owner, event.Name, event.Description, event.Location, event.DateTime).Scan(&event.Id)
 }
 
 func (em *EventModel) GetAll() ([]*Event, error) {
@@ -53,7 +53,7 @@ func (em *EventModel) GetAll() ([]*Event, error) {
 	for rows.Next() {
 		var event Event
 
-		err := rows.Scan(&event.Id, &event.Owner, &event.Name, &event.Description, &event.Location, &event.Date)
+		err := rows.Scan(&event.Id, &event.Owner, &event.Name, &event.Description, &event.Location, &event.DateTime)
 
 		if err != nil {
 			return nil, err
@@ -78,7 +78,7 @@ func (em *EventModel) Get(id int) (*Event, error) {
 
 	var event Event
 
-	err := em.DB.QueryRowContext(ctx, query, id).Scan(&event.Id, &event.Owner, &event.Name, &event.Description, &event.Location, &event.Date)
+	err := em.DB.QueryRowContext(ctx, query, id).Scan(&event.Id, &event.Owner, &event.Name, &event.Description, &event.Location, &event.DateTime)
 
 	if err != nil {
 		if err == sql.ErrNoRows {
@@ -88,4 +88,36 @@ func (em *EventModel) Get(id int) (*Event, error) {
 	}
 
 	return &event, nil
+}
+
+func (em *EventModel) Update(event *Event) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+
+	defer cancel()
+
+	query := "UPDATE events SET name = $1, description = $2, location = $3, date = $4 WHERE id = $5"
+
+	_, err := em.DB.ExecContext(ctx, query, event.Name, event.Description, event.Location, event.DateTime, event.Id)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (em *EventModel) Delete(id int) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+
+	defer cancel()
+
+	query := "DELETE FROM events WHERE id = $1"
+
+	_, err := em.DB.ExecContext(ctx, query, id)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
