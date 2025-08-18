481798b055bbf517c433d8e51e5a91952722a1c4
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2025-08-18 09:25:32 +0530

    feat(user): api routes for insert and get user

--- internal/database/users.go
+++ internal/database/users.go
@@ -1,6 +1,12 @@
 package database
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	"time"
+
+	"github.com/shekharsikku/go-gin-rest-api/internal/utils"
+)
 
 type UserModel struct {
 	DB *sql.DB
@@ -8,7 +14,84 @@ type UserModel struct {
 
 type User struct {
 	Id       int    `json:"id"`
-	Email    string `json:"email"`
 	Name     string `json:"name"`
+	Email    string `json:"email"`
 	Password string `json:"-"`
+}
+
+func (em *UserModel) Insert(user *User) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+
+	defer cancel()
+
+	query := "INSERT INTO users (id, name, email, password) VALUES ($1, $2, $3, $4) RETURNING id"
+
+	id := utils.GenerateUniqueID()
+
+	return em.DB.QueryRowContext(ctx, query, id, user.Name, user.Email, user.Password).Scan(&user.Id)
+}
+
+func (em *UserModel) getUser(query string, args ...any) (*User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	
+	defer cancel()
+
+	var user User
+
+	err := em.DB.QueryRowContext(ctx, query, args...).Scan(&user.Id, &user.Name, &user.Email, &user.Password)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (m *UserModel) Get(id int) (*User, error) {
+	query := "SELECT * FROM users WHERE id = $1"
+	return m.getUser(query, id)
+}
+
+func (m *UserModel) GetByEmail(email string) (*User, error) {
+	query := "SELECT * FROM users WHERE email = $1"
+	return m.getUser(query, email)
+}
+
+func (em *UserModel) GetAll() ([]*User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	
+	defer cancel()
+
+	query := "SELECT * FROM users"
+
+	rows, err := em.DB.QueryContext(ctx, query)
+	
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	users := []*User{}
+
+	for rows.Next() {
+		var user User
+
+		err := rows.Scan(&user.Id, &user.Name, &user.Email, &user.Password)
+		
+		if err != nil {
+			return nil, err
+		}
+
+		users = append(users, &user)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return users, nil
 }
